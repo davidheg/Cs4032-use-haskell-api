0339aaa7d5aecf0242f6cab8490312cb549363e6
davidheg <david.hegarty.9@gmail.com>
2017-01-18 20:34:34 +0000

    Added searchFile method

--- src/UseHaskellAPIClient.hs
+++ src/UseHaskellAPIClient.hs
@@ -29,4 +29,4 @@ uploadFile :: UserFile -> ClientM Bool
 -- | The following provides the implementations of these types
 -- Note that the order of the functions must match the endpoints in the type API from UseHaskell.hs
 
-(loadEnvVars :<|> getREADME :<|> storeMessage :<|> searchMessage :<|> performRestCall :<|> uploadFile) = client restAPI
+(loadEnvVars :<|> getREADME :<|> storeMessage :<|> searchMessage :<|> performRestCall :<|> uploadFile :<|> searchFile) = client restAPI
